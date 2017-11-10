353fa1c64978852731d6d53cf0b38cc44c5acfd1
zedany7@hotmail.com <Alk0544282120>
2017-11-10 18:02:08 +0200

    carousel home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:string = 'LoginPage';
+  rootPage:string = 'TabsPage';
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
---
+++ src/components/carousel/carousel.component.ts
@@ -0,0 +1,40 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {ServiceProvider} from "../../models/service-provider/service-provider.interface";
+import {ServiceProvidersProvider} from "../../providers/service-providers/service-providers";
+import { ENTERPRISE_LIST } from '../../mocks/enterprise/enterprise';
+import {NavController} from "ionic-angular";
+import {Enterprise} from "../../models/enterprise/enterprise";
+
+/**
+ * Generated class for the ServiceProviderComponent component.
+ *
+ * See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
+ * for more info on Angular Components.
+ */
+@Component({
+  selector: 'carousel-ah',
+  templateUrl: 'carousel.component.html'
+})
+export class CarouselComponent implements OnInit{
+   serviceProvidersList: ServiceProvider[];
+   loadPrimary: boolean=true;
+  enterpriseList: Enterprise[] = ENTERPRISE_LIST;
+  constructor(private navCtrl: NavController, private serviceProvidersProvider:ServiceProvidersProvider) {
+    console.log(ENTERPRISE_LIST)
+  }
+
+  ngOnInit(){
+    this.serviceProvidersProvider.getServiceProvidersList({ primary: true }).subscribe((data: ServiceProvider[]) => this.serviceProvidersList = data);
+  }
+  navigateToServiceProvider(selectedServiceProvider){
+    this.navCtrl.push('ServiceProviderPage', {selectedServiceProvider: selectedServiceProvider});
+  }
+  //
+  // navigateToServiceProvider(selectedServiceProvider){
+  //   this.navCtrl.push('ServiceProviderPage', {selectedServiceProvider: selectedServiceProvider});
+  // }
+  //
+
+
+
+}
